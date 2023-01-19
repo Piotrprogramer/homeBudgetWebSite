082016b80ea254be920c7fe2df573c7bb95e2c0b
Piotr <piotr.wasilewski.programista@gmail.com>
2023-01-19 20:31:38 +0100

    save user file to DB

--- save.php
+++ save.php
@@ -1,3 +1,42 @@
+<?php
+
+session_start();
+
+if (isset($_POST['login']) && isset($_POST['password']) && isset($_POST['repeatPassword'])) {
+	$login = $_POST['login'];
+	$password = $_POST['password'];
+	$repeatPassword = $_POST['repeatPassword'];
+	//$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+	
+	//if (empty($email)) {
+		
+	//	$_SESSION['given_email'] = $_POST['email'];
+	//	header('Location: index.php');
+		
+	//} else {
+	if($password == $repeatPassword){
+		require_once 'database.php';
+		
+		$query = $db->prepare('INSERT INTO uzytkownicy VALUES (NULL, :login, :password)');
+		$query->bindValue(':login', $login, PDO::PARAM_STR);
+		$query->bindValue(':password', $password, PDO::PARAM_STR);
+		$query->execute();
+	}
+	else {
+		$_SESSION['given_login'] = $_POST['login'];
+		header('Location: Rejestracja.html');
+		exit();
+	}
+	//}
+	
+	
+} else {
+	
+	header('Location: Rejestracja.html');
+	exit();
+}
+
+?>
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
@@ -27,25 +66,14 @@
 				Zadbaj o swój portfel
 			</div>
 		</div>
-		<div class="row justify-content-center">
-		
-			<div id="register_frame">
-				<form method="post" action="save.php">
-					<input type="text" name="login" placeholder="login" onfocus="this.placeholder=''" onblur="this.placeholder='login'" required>
-					
-					<input type="password" name="password" placeholder="hasło" onfocus="this.placeholder=''" onblur="this.placeholder='hasło'" required>
-					
-					<input type="password" name="repeatPassword" placeholder="powtórz hasło" onfocus="this.placeholder=''" onblur="this.placeholder='powtórz hasło'" required>
-					
-					<input type="submit" value="Zarejestruj się">
-					
-					<a href="Index.html"><div class="log_button" style="margin-top:10px">Powrót</div> </a>
-				</form>
-			</div>
+		<div class="row justify-content-center" id="content">
+			<div >
+				<div >Gratulacje, konto założone pomyślnie!</div>	
+				<a href="Index.html"><div class="log_button" style="margin-top:10px">Powrót</div> </a>
+			</div>	
 		</div>
-		
 	</div>
-	
+
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
